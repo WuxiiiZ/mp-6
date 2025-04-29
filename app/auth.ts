4e87e94f0f518c1df15ce7e8dc64ab43f8ad8ecb
adc688 <alicewx@bu.edu>
2025-04-29 03:14:58 -0400

    update

--- app/auth.ts
+++ app/auth.ts
@@ -1,13 +1,17 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
+console.log("GITHUB_CLIENT_ID:", process.env.GITHUB_CLIENT_ID);
+console.log("GITHUB_CLIENT_SECRET:", process.env.GITHUB_CLIENT_SECRET);
+console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
+
 export const {handlers, auth} =NextAuth(
     {
         providers:[
             GitHub({
                 clientId:process.env.GITHUB_CLIENT_ID,
                 clientSecret:process.env.GITHUB_CLIENT_SECRET,
-                authorization: {params: {scope: "read:user user:email"}}
+                authorization: {params: {scope: "read:user user:email"}},
             })
         ],
         session:{
